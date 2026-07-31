bb3a9a6da3de556b412a8461331598a7d50322f5
Samruddhi Tribhan <147504352+SamruddiTribhan@users.noreply.github.com>
2026-07-31 15:30:28 +0530

    Time: 9 ms (94.22%), Space: 47.9 MB (73.7%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumPushes(String word) {
+       int[] freq = new int[26] ;
+
+       for(char ch : word.toCharArray())
+            freq[ch-'a']++;
+
+        Arrays.sort(freq);
+        int push =0;
+        int count =0;
+        int cost =1;
+
+        for(int i=25; i>=0;i--){
+            if(freq[i]==0) break;
+
+            push +=freq[i]*cost;
+
+            count++;
+
+            if(count%8==0) cost++;
+
+        }
+        return push;
+
+       
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
